41ff6c5a9ae60d851f8819a7f85e4a799bf9379a
stambata <stamen.peev@softwaregroup-bg.com>
2022-04-12 13:16:09 +0300

    test: add test (#3)
    Co-authored-by: Stamen Peev <stamen.peev@softwaregroup.com>

---
+++ tap-snapshots/test/index.test.js.test.cjs
@@ -0,0 +1,17 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/index.test.js TAP jobs port.test server tests subjectObjectPredicate > must match snapshot 1`] = `
+Object {
+  "$meta": Object {},
+  "id": 1,
+  "jsonrpc": "2.0",
+  "result": Object {
+    "test": true,
+  },
+}
+`
--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,85 @@
-require('../');
-const tap = require('tap');
-tap.pass('no tests yet');
+const got = require('got');
+const keys = require('../keys');
+require('ut-run').run({
+    main: [
+        () => ({
+            test: () => [
+                require('../dispatch'),
+                (...params) => require('ut-function.dispatch')({
+                    namespace: ['subject', 'login'],
+                    methods: {
+                        'subject.object.predicate': msg => msg,
+                        'login.oidc.mle'() {
+                            const {sign, encrypt} = this.bus.info();
+                            return {sign, encrypt};
+                        }
+                    }
+                })(...params),
+                function validation() {
+                    return [
+                        ({ joi }) => ({
+                            'subject.object.predicate': () => ({
+                                description: 'subject object predicate',
+                                params: joi.object(),
+                                result: joi.object(),
+                                auth: 'exchange'
+                            }),
+                            'login.oidc.mle': () => ({
+                                method: 'GET',
+                                path: '/.well-known/mle',
+                                auth: false
+                            })
+                        })
+                    ];
+                }
+            ]
+        })
+    ],
+    method: 'unit',
+    config: {
+        test: true,
+        utBus: {
+            serviceBus: {
+                jsonrpc: {
+                    ...keys
+                }
+            }
+        },
+        babelfish: {
+            server: {
+                port: 8091
+            },
+            mle: {
+                routes: [{
+                    path: '/rpc/subject/object/predicate',
+                    auth: 'exchange'
+                }]
+            },
+            utLog: {
+                streams: {
+                    udp: false
+                }
+            }
+        }
+    },
+    params: {
+        steps: [
+            {
+                name: 'subjectObjectPredicate',
+                params: () => got.post('http://localhost:8091/rpc/subject/object/predicate', {
+                    json: {
+                        jsonrpc: '2.0',
+                        id: 1,
+                        method: 'subject.object.predicate',
+                        params: {
+                            test: true
+                        }
+                    }
+                }).json(),
+                result(msg, assert) {
+                    assert.matchSnapshot(msg);
+                }
+            }
+        ]
+    }
+});
